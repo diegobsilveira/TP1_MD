ffb9964671b92822b742fb31c2dbdd0f83127dc8
Diego Barros <diegobsilveira@gmail.com>
2017-05-31 11:56:36 -0300

    Add files via upload

--- analise3.c
+++ analise3.c
@@ -30,56 +30,36 @@ void imprimeSequencia(int r, int* sequencia) {
 	int i;
 	for(i = 0; i < r; i++)
 		printf("%d ", *(sequencia + i));
-}
-
-
-int proximaPermutacao(int* permutacao, int r, int n){
-    int i = 0, j;
-    
-    if(permutacao[i] == n){ //checa se a o algarismo menos significativo na permutação chegou ao limite 'n'
-        for(i = i + 1;permutacao[i] >= n && i < r; i++); // encontra o primeiro algarismo menor que o limite 'n'
-        if(i < r){ //checa se as permutacoes nao acabaram
-            permutacao[i]++;
-            for(j = 0; j < i; j++){ // iguala todas as casas a direita do algarismo a ele
-                permutacao[j] = permutacao[i];
-            }
-        }
-        else{
-            return 0; // nao existe proxima permutacao
-        }
-    }
-    else{
-        permutacao[i]++;
-    }
-    return 1; //existe permutacao
+        
+    printf("\n");
 }
 
 void combinaRepet(int r, int n) {
 	int i, j, k;
 	int* combinacao = inicializaSequencia(r);
-	
+
 	//Como é um vetor, o elemento mais à direita é o 'algarismo menos significativo' na hora de imprimir
 	for(i = r - 1; i >= 0; i--) {						//Vai do algarismo 'menos significativo' até o 'mais significativo'
-	    for(j = r - 1; j >= i; j--) {
-	        while((*(combinacao + j)) < n) {
-		        imprimeSequencia(r, combinacao);
-		        printf("\n");
-		        
-		        (*(combinacao + j))++;
-	        }
+	    for(j = r - 1; j >= i; j--) {                   //Vai do algarismo 'menos significativo' até a posição atual ('i') e
+	        while(combinacao[r - 1] <= n) { 
+	            imprimeSequencia(r, combinacao);
+                fflush(stdout);
+                
+                combinacao[r - 1]++;
+            }
+            if(i > 0) {
+                combinacao[i - 1]++; 
+                for(k = i; k < r; k++)                            //Para todas as posições anterioes
+                    combinacao[k] = combinacao[i - 1];                //Iguala elas à atual, até a última (mais a direita) ao valor da próxima incrementada.
+            } 
 	    }
-		
-		for(k = i; k <= (r - 1); k++)
-			(*(combinacao + k)) = (*(combinacao + (i - 1))) + 1;
-			
-		(*(combinacao + (i - 1)))++;
-	}
+    }
 }
 
 int main(int argc, char* argv[]) {
 	int r, n;	//Variáveis definidas pelo input do usuário, 
-			//de  controle, para imprimir a permutação com
-			//repetição para o usuário.
+    			//de  controle, para imprimir a permutação com
+    			//repetição para o usuário.
 
 	printf("Digite o valor de r (2 <= r <= 10) e n (1 <= n <= 5) respectivamente: \n");
 	scanf("%d%d", &r, &n);
